02e15814df5d6f72ace45b4b0e44a47afc3461b5
omerbakr <y.omerbakir@gmail.com>
2026-02-27 16:48:25 +0300

    feat: set up initial routes

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ import express from "express";
 
 import { PORT } from "./config/env.js";
 
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
